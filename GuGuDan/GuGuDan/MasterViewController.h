0a82df0158ca592e8bdca841bf51b9840784437e
Kevin Kwon <myhhkb@gmail.com>
2014-07-17 00:26:00 +0900

    gugudan init

---
+++ GuGuDan/GuGuDan/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  GuGuDan
+//
+//  Created by hdk on 2014. 7. 17..
+//  Copyright (c) 2014년 Kevin Kwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
